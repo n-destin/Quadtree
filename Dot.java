311c7846fbe13fe90eb85380ee70099fb1487338
n-destin <111414464+n-destin@users.noreply.github.com>
2023-04-04 23:03:34 -0400

    Add files via upload

---
+++ Dot.java
@@ -0,0 +1,33 @@
+/**
+ * Dot
+ * Problem Set 2
+ * @author Takudzwa Clive Mutombo
+ * @author Destin Niyomufasha
+ */
+public class Dot implements Point2D {
+	private double x, y;
+	public Dot(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String toString() {
+		return "("+x+","+y+")";
+	}
+}
---
+++ Point2D.java
@@ -0,0 +1,12 @@
+/**
+ * Point2D
+ * Problem Set 2
+ * @author Takudzwa Clive Mutombo
+ * @author Destin Niyomufasha
+ */
+public interface Point2D {
+	public double getX();
+	public double getY();
+	public void setX(double newX);
+	public void setY(double newY);
+}
